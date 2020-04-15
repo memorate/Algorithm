923cd49b4242dfdda987551fd16945d041ab328b
mawen <mawen@meicloud.com>
2020-04-15 08:40:55 +0800

    增加ImplementStr

---
+++ src/main/java/ImplementStr.java
@@ -0,0 +1,37 @@
+/**
+ * 实现 strStr() 函数。
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ *
+ * 示例 1:
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ *
+ * 示例 2:
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-strstr
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ImplementStr {
+
+    public static int MySolution(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        return haystack.indexOf(needle);
+    }
+
+    /**
+     * 沿着字符换逐步移动滑动窗口，将窗口内的子串与 needle 字符串比较。
+     */
+    public int LeetCodeSolution(String haystack, String needle) {
+        int L = needle.length(), n = haystack.length();
+
+        for (int start = 0; start < n - L + 1; ++start) {
+            if (haystack.substring(start, start + L).equals(needle)) {
+                return start;
+            }
+        }
+        return -1;
+    }
+}
