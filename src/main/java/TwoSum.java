0f986582b14a31b3516183152c18b14d8c1d099f
Anchor <sweetsorrow211@gmail.com>
2020-09-02 18:29:59 +0800

    add "Sqrt(x)"

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
  * 链接：https://leetcode-cn.com/problems/two-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class One_TwoSum {
+public class TwoSum {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
