2ff854a1fe2ff67f1b7d05328acd4427e2e7a99b
Anchor <sweetsorrow211@gmail.com>
2020-09-02 18:25:28 +0800

    update "add binary"

--- src/main/java/AddBinary.java
+++ src/main/java/AddBinary.java
@@ -20,55 +20,90 @@
 public class AddBinary {
 
     public static void main(String[] args) {
-
+        System.out.println(MySolution("11", "1"));
+        System.out.println(MySolution("1010", "1011"));
+        System.out.println(MySolution("0", "0"));
+        System.out.println(MySolution("11111", "11111111"));
     }
 
+    /**
+     * 大致思路：
+     * 用双指针i、j来定位a、b的位，从末位开始i、j相加，carry用来进位
+     * 当较小数被加完后，再对较大数的剩余位和carry进行运算
+     *
+     * 判断是否进位的条件：
+     * 将i、j所在的位转为int与carry相加，结果只会为0、1、2、3，分别代表（0,0,0）、（0,1,0）、（0,1,1）、（1,1,1）
+     */
     public static String MySolution(String a, String b) {
-        if (a.length() < b.length()){
+        //a一定是较大数，若不是，手动将a替换为较大数
+        if (a.length() < b.length()) {
             String temp = a;
             a = b;
             b = temp;
         }
-        int big = Math.max(a.length(), b.length());
-        int small = Math.min(a.length(), b.length());
-        char[] result = new char[big + 1];
-        char[] carry = new char[]{'0', '0', '0'};
-        int index = big - 1;
-        for (; index >= 0; index--) {
-            if (index == small - 1) {
-                break;
-            } else {
-                carry[0] = a.charAt(index);
-                carry[1] = b.charAt(index);
-                int counter = 0;
-                for (char item : carry) {
-                    if (item == '1') counter++;
-                }
-                switch (counter) {
+        int carry = 0;           //控制是否进位
+        String r = "";           //结果
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0; i--) {        //i、j是双指针
+            int aa = a.charAt(i) == '0' ? 0 : 1, bb;     //aa —— 较大数的当前位，bb —— 较小数的当前位（aa、bb非0即1）
+            if (j >= 0) {      //控制较小数是否已被加完
+                bb = b.charAt(j) == '0' ? 0 : 1;         //bb赋值动作放在这是为了防止 out of range
+                switch (aa + bb + carry) {
                     case 0:
-                        result[index + 1] = '0';
-                        carry[2] = '0';
+                        r = 0 + r;
+                        carry = 0;
                         break;
                     case 1:
-                        result[index + 1] = '1';
-                        carry[2] = '0';
+                        r = 1 + r;
+                        carry = 0;
                         break;
                     case 2:
-                        result[index + 1] = '0';
-                        carry[2] = '1';
+                        r = 0 + r;
+                        carry = 1;
                         break;
                     case 3:
-                        result[index + 1] = '1';
-                        carry[2] = '1';
+                        r = 1 + r;
+                        carry = 1;
+                        break;
+                }
+                j--;
+            } else {     //控制较大数加完较小数后剩余位的运算
+                switch (aa + carry) {
+                    case 0:
+                        r = 0 + r;
+                        carry = 0;
+                        break;
+                    case 1:
+                        r = 1 + r;
+                        carry = 0;
+                        break;
+                    case 2:
+                        r = 0 + r;
+                        carry = 1;
                         break;
                 }
             }
         }
-        return "";
+        r = carry == 0 ? r : 1 + r;
+        return r;
     }
 
-    public static String LeetCodeSolution(String a, String b) {
 
-        return "";
+    public static String LeetCodeSolution(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
     }
 }
