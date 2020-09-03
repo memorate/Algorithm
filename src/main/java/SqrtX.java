a148371fbb8e9ce2ad56b617678431cdc83c6564
Anchor <sweetsorrow211@gmail.com>
2020-09-03 18:29:11 +0800

    update 69.【SqrtX】

--- src/main/java/SqrtX.java
+++ src/main/java/SqrtX.java
@@ -2,11 +2,11 @@
  * 69.【实现 int sqrt(int x) 函数】
  * 计算并返回 x 的平方根，其中 x 是非负整数。
  * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
- *
+ * <p>
  * 示例 1:
  * 输入: 4
  * 输出: 2
- *
+ * <p>
  * 示例 2:
  * 输入: 8
  * 输出: 2
@@ -18,7 +18,13 @@ public class SqrtX {
     }
 
     static int MySolution(int x) {
+        int left = 0, right = x / 2;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid * mid < x){
 
+            }
+        }
         return 0;
     }
 
