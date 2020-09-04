ad6f4201663027d5851093fc401f56c0eeea4bca
Anchor <sweetsorrow211@gmail.com>
2020-09-04 18:28:26 +0800

    add 83.【RemoveDuplicateFromSortedList】

---
+++ src/main/java/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,33 @@
+/**
+ * 83.【删除排序链表中的重复元素】
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ *
+ * 示例1:
+ * 输入: 1->1->2
+ * 输出: 1->2
+ *
+ * 示例2:
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ */
+public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+
+    }
+
+    static ListNode MySolution(ListNode head) {
+
+        return null;
+    }
+
+    static ListNode LeetCodeSolution(ListNode head) {
+
+        return null;
+    }
+
+    static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+}
