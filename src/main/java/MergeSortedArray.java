3eef8e26f04a884ede5dcaa408797b5ab77c0ae6
Anchor <sweetsorrow211@gmail.com>
2020-09-08 18:03:38 +0800

    complete 88.【MergeSortedArray】

--- src/main/java/MergeSortedArray.java
+++ src/main/java/MergeSortedArray.java
@@ -1,3 +1,7 @@
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+import java.util.Arrays;
+
 /**
  * 88.【合并两个有序数组】
  * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
@@ -16,14 +20,75 @@
 public class MergeSortedArray {
 
     public static void main(String[] args) {
+        int[] m1 = {4, 5, 6, 0, 0, 0};
+        int[] m2 = {1, 2, 3};
+//        MySolution(m1, 3, m2, 3);
+        LeetCodeSolution1(m1, 3, m2, 3);
 
+        int[] n1 = {1, 2, 3, 0, 0, 0};
+        int[] n2 = {2, 5, 6};
+//        MySolution(n1, 3, n2, 3);
+        LeetCodeSolution1(n1, 3, n2, 3);
+
+        int[] o1 = {2, 0};
+        int[] o2 = {1};
+//        MySolution(o1, 1, o2, 1);
+        LeetCodeSolution1(o1, 1, o2, 1);
+
+        int[] p1 = {0};
+        int[] p2 = {1};
+//        MySolution(p1, 0, p2, 1);
+        LeetCodeSolution1(p1, 0, p2, 1);
     }
 
     static void MySolution(int[] nums1, int m, int[] nums2, int n) {
-
+        int x = m - 1, y = n - 1;
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (x < 0) {
+                nums1[i] = nums2[y];
+                y--;
+                continue;
+            }
+            if (y < 0) break;
+            if (nums1[x] >= nums2[y]) {
+                nums1[i] = nums1[x];
+                x--;
+            } else {
+                nums1[i] = nums2[y];
+                y--;
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
     }
 
-    static void LeetCodeSolution(int[] nums1, int m, int[] nums2, int n) {
+    /**
+     * 秀啊
+     */
+    static void LeetCodeSolution1(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-- + --n;
+        while (n >= 0) {
+            nums1[i--] = m >= 0 && nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
 
+    /**
+     * 官方解答
+     */
+    static void LeetCodeSolution2(int[] nums1, int m, int[] nums2, int n) {
+        // two get pointers for nums1 and nums2
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // set pointer for nums1
+        int p = m + n - 1;
+
+        // while there are still elements to compare
+        while ((p1 >= 0) && (p2 >= 0))
+            // compare two elements from nums1 and nums2
+            // and add the largest one in nums1
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+        // add missing elements from nums2
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
     }
 }
