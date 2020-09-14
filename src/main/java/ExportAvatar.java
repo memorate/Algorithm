4455d46efd32b690f6b687a759bcab3a2b221d96
Anchor <sweetsorrow211@gmail.com>
2020-09-14 17:41:51 +0800

    execute by thread

--- src/main/java/ExportAvatar.java
+++ src/main/java/ExportAvatar.java
@@ -7,55 +7,82 @@ import cn.hutool.http.HttpUtil;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class ExportAvatar {
-    public static void main(String[] args) throws SQLException {
+
+    final static String destination = "C:\\Users\\ancho\\Desktop\\avatar\\";
+
+    public static void main(String[] args) throws Exception {
+        downloadDept();
+    }
+
+    static int downloadUser() throws Exception{
         Db db = Db.use();
-        List<Entity> list = db.findAll(Entity.create("muc_dept").set("dept_name_path", dept()).set("is_removed", 0));
+        List<Entity> list = db.query(userSql(userInitialize()));
+        for (Entity entity : list) {
+            String uid = (String) entity.get("uid");
+            String headPhoto = (String) entity.get("head_photo");
+            try {
+                download2(uid, headPhoto);
+            } catch (Exception e) {
+                String log = "download fail.uid = %s.url = %s.";
+                System.out.println(String.format(log, uid, headPhoto));
+            }
+        }
+        System.out.println(list);
+        return list.size();
+    }
+
+    static int downloadDept() throws Exception{
+        Db db = Db.use();
+        List<Entity> list = db.findAll(Entity.create("muc_dept").set("dept_name_path", deptInitialize()).set("is_removed", 0));
         List<String> deptIdPath = list.stream().map(t -> (String) t.get("dept_id_path")).collect(Collectors.toList());
-        List<Entity> result = new ArrayList<>();
+        List<Entity> allAvatar = new ArrayList<>();
         for (String s : deptIdPath) {
-            List<Entity> query = db.query(sql(), s + "%");
-            result.addAll(query);
+            List<Entity> query = db.query(deptSql(), s + "%");
+            allAvatar.addAll(query);
         }
         db.closeConnection(db.getConnection());
         System.out.println("——————————Starting download————————————");
-        System.out.println("Estimate download num: " + result.size());
-        int success = 0;
-        AtomicInteger fail = new AtomicInteger();
-        Set<String> exclude = new HashSet<>();
-        for (Entity entity : result) {
-            String uid = (String) entity.get("uid");
-            String headPhoto = (String) entity.get("head_photo");
-            exclude.add(uid);
-            ThreadUtil.execute(()-> {
-                try {
-                    download2(uid, headPhoto);
-                } catch (Exception e) {
-                    String log = "download fail.uid = %s.url = %s.";
-                    System.out.println(String.format(log, uid, headPhoto));
-                    fail.getAndIncrement();
+        System.out.println("Estimate download num: " + allAvatar.size());
+        AtomicInteger success = new AtomicInteger(), fail = new AtomicInteger();
+        List<List<Entity>> downloadList = subList(allAvatar, 200);
+        for (List<Entity> item : downloadList) {
+            ThreadUtil.execute(() -> {
+                System.out.println(Thread.currentThread().getName() + " start downloading!");
+                for (Entity entity : item) {
+                    String uid = (String) entity.get("uid");
+                    String headPhoto = (String) entity.get("head_photo");
+                    try {
+                        download2(uid, headPhoto);
+                    } catch (Exception e) {
+                        String log = "download fail.uid = %s.url = %s.";
+                        System.out.println(String.format(log, uid, headPhoto));
+                        fail.getAndIncrement();
+                    }
+                    success.getAndIncrement();
                 }
             });
-            success++;
         }
         System.out.println("————————————Statistic————————————");
-        System.out.println("Total download: " + (success + fail.get()));
-        System.out.println("Truly total: " + exclude.size());
+        System.out.println("Total download: " + (success.get() + fail.get()));
         System.out.println("Successful download: " + success);
         System.out.println("Failed download: " + fail);
-        System.out.println("————————end————————");
+        System.out.println("———————————————end———————————————");
+        return success.get();
     }
 
-    static List<String> dept() {
+    static List<String> userInitialize(){
+//        String[] array = {"fanghb","wangjg","huziqiang","xiaomg","zhangxy11","lign",
+//                "francoise","btyin","helmut.zodl","guym","wangjl"};
+        String[] array = {"funyj", "zhaolei2", "scott", "peter", "avant", "xumf", "macp"};
+        return ListUtil.toList(array);
+    }
+
+    static List<String> deptInitialize() {
         List<String> dept = new ArrayList<>();
         dept.add("美的_美的集团_家用空调事业部");
         dept.add("美的_美的集团_生活电器事业部");
@@ -67,7 +94,19 @@ public class ExportAvatar {
         return dept;
     }
 
-    static String sql() {
+    static String userSql(List<String> users){
+        String joint = "";
+        for (String user : users) {
+            joint += "'" + user + "',";
+        }
+        joint = joint.substring(0, joint.lastIndexOf(','));
+        return "select emp.uid, ext.head_photo\n" +
+                     "from muc_emp_ext ext,\n" +
+                     "     muc_emp emp\n" +
+                     "where emp.uid in (" + joint + ") and emp.id = ext.emp_id";
+    }
+
+    static String deptSql() {
         return "with a as (select emp.id, emp.uid " +
                           "from muc_dept dept, " +
                                "muc_emp_dept_rel dept_rel, " +
@@ -88,16 +127,25 @@ public class ExportAvatar {
         HttpUtil.downloadFile(url, String.format(dir, fileName));
     }
 
-    static void download2(String fileName, String url) throws Exception{
-        String dir = "C:\\Users\\ancho\\Desktop\\avatar\\%s.jpg";
-        URL url1 = new URL(url);
-        URLConnection uc = url1.openConnection();
-        InputStream inputStream = uc.getInputStream();
+    static void download2(String fileName, String avatarUrl) throws Exception{
+        String dir = destination + "%s.jpg";
+        URL url = new URL(avatarUrl);
+        InputStream inputStream = url.openConnection().getInputStream();
         FileOutputStream out = new FileOutputStream(String.format(dir, fileName));
         int j = 0;
         while ((j = inputStream.read()) != -1) {
             out.write(j);
         }
         inputStream.close();
+    }
+
+    static <T> List<List<T>> subList(List<T> list, int step) {
+        if (list == null || list.size() == 0 || step < 1) return Collections.emptyList();
+        List<List<T>> result = new ArrayList<>();
+        for (int i = 0; i < list.size(); ) {
+            List<T> subList = list.subList(i, i = Math.min(i + step, list.size()));
+            result.add(subList);
+        }
+        return result;
     }
 }
---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        List<String> r = new ArrayList<>();
+        r.add("a");
+        r.add("b");
+        r.add("c");
+        r.add("d");
+        r.add("e");
+        System.out.println(r);
+    }
+}
