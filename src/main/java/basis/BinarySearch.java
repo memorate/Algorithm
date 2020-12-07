23ee097aa9b5936fc809b483b00cd095840897cb
Anchor <sweetsorrow211@gmail.com>
2020-12-07 18:24:33 +0800

    optimize ConvertSortedArrayToBiSearchTree.java add BinarySearch.java

---
+++ src/main/java/basis/BinarySearch.java
@@ -0,0 +1,18 @@
+package basis;
+
+/**
+ * 二分法
+ *
+ * https://www.cnblogs.com/kyoner/p/11080078.html
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int array[] = {1, 3, 4, 9, 12, 18, 21, 33, 42, 62, 94};
+    }
+
+    private static int biSearch(){
+
+        return 0;
+    }
+}
--- src/main/java/simple/ConvertSortedArrayToBiSearchTree.java
+++ src/main/java/simple/ConvertSortedArrayToBiSearchTree.java
@@ -40,7 +40,7 @@ public class ConvertSortedArrayToBiSearchTree {
 
     private static TreeNode converter(int[] nums, int left, int right) {
         if (left > right) return null;
-        int mid = (left + right) / 2;
+        int mid = left + (right - left) / 2;
         TreeNode root = new TreeNode(nums[mid]);
         root.left = converter(nums, left, mid - 1);
         root.right = converter(nums, mid + 1, right);
