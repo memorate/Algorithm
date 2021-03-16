a058563e9fd58091442b49ef92ee8209b5dd78e1
Anchor <sweetsorrow211@gmail.com>
2021-03-16 17:59:16 +0800

    add Bubble and Select

---
+++ src/main/java/sort/Bubble.java
@@ -0,0 +1,38 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * @author Anchor
+ *
+ * 冒泡排序：
+ *   for{
+ *       1.前后俩元素对比，将较大的元素放在后面;
+ *       2.一次 for 循环结束后，数组中最大的元素在末尾;
+ *       3.将末尾元素剔除，再执行第一步;
+ *   }
+ *
+ * 时间复杂度: O(n²)
+ * 空间复杂度: O(1)
+ * 稳定
+ */
+public class Bubble {
+    public static void main(String[] args) {
+        int[] array = {9, 3, 7, 6, 1, 7, 4, 5};
+        System.out.println(Arrays.toString(bubble(array)));
+    }
+
+    private static int[] bubble(int[] array) {
+        if (array == null || array.length == 0) return new int[0];
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
---
+++ src/main/java/sort/Select.java
@@ -0,0 +1,38 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * @author Anchor
+ *
+ * 选择排序：
+ *   1.定位到当前数组中最大的元素，将其与末尾元素互换
+ *   2.排除数组末尾已排序的元素，继续执行第一步
+ *
+ * 时间复杂度: O(n²)
+ * 空间复杂度: O(1)
+ * 不稳定
+ */
+public class Select {
+    public static void main(String[] args) {
+        int[] array = {9, 3, 7, 6, 1, 7, 4, 5};
+        System.out.println(Arrays.toString(select(array)));
+    }
+
+    private static int[] select(int[] array) {
+        if (array == null || array.length == 0) return new int[0];
+        for (int i = array.length - 1; i > 0; i--) {
+            int max = array[0], index = 0;
+            for (int j = 0; j <= i; j++) {
+                if (array[j] > max) {
+                    max = array[j];
+                    index = j;
+                }
+            }
+            int temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+}
